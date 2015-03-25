d06eafce77ca761567d114c073136ea534cb5fe7
hbod8 <harrysaliba4139@gmail.com>
2015-03-24 19:52:00 -0700

    Create MessageServer.java

---
+++ MessageServer.java
@@ -0,0 +1,64 @@
+//NOTE: This is the non-working part of the project, the InputStream between the MessageThreads are not connected.
+
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class MessageServer {
+
+    static final int PORT = 1234;
+
+    public static void main(String args[]) {
+        ServerSocket serverSocket = null;
+        Socket socket = null;
+
+        try {
+            serverSocket = new ServerSocket(PORT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        while (true) {
+            try {
+                socket = serverSocket.accept();
+            } catch (IOException e) {
+                System.out.println("I/O error: " + e);
+            }
+            // new thread for a client
+            new MessageThread(socket).start();
+        }
+    }
+}
+
+class MessageThread extends Thread {
+    protected Socket socket;
+    public MessageThread(Socket clientSocket) {
+        this.socket = clientSocket;
+    }
+    public void run() {
+        InputStream inp = null;
+        BufferedReader brinp = null;
+        DataOutputStream out = null;
+        try {
+            inp = socket.getInputStream();
+            brinp = new BufferedReader(new InputStreamReader(inp));
+        } catch (IOException e) {
+            return;
+        }
+        String line;
+        while (true) {
+            try {
+                line = brinp.readLine();
+                System.out.print(line + "\n");
+                if ((line == null) || line.equalsIgnoreCase("QUIT")) {
+                    socket.close();
+                    return;
+                } else {
+                    out.writeBytes(line + "\n\r");
+                    out.flush();
+                }
+            } catch (IOException e) {
+                return;
+            }
+        }
+    }
+}
