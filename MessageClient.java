355ac52a91c2ce91e813deab23b3f9507a8c5d87
Harry Saliba <harrysaliba4139@gmail.com>
2015-06-10 17:53:53 -0700

    BIG UPDATE: now works, kinda

--- MessageClient.java
+++ MessageClient.java
@@ -1,107 +1,130 @@
-//Client for JavaMessage
-//
-//Plans:
-//
-//Make a better GUI, ie colors and stuff...
-
-import java.net.*;
 import java.io.*;
 import java.util.*;
-import javax.swing.*;
+import java.net.*;
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.*;
 
-class MessageClient {
-    public static final int PORT = 1234;
+public class MessageClient {
     public static void main(String[] args) {
-        Socket socket = null;
-        System.out.println("-- Message System Started --");
-        System.out.println("INFO:Opening port " + PORT);
-        try {
-            InetAddress ip = InetAddress.getByName("24.17.105.4");
-            socket = new Socket(ip, PORT);
-        } catch(ConnectException e) {
-            System.err.println("Server is down! " + e);
-            System.exit(-1);
-        } catch(Exception e) {
-            System.err.println(e);
-        }
-        SwingUtilities.invokeLater(new MessageGUI(socket));
-    }
-}
-
-class MessageGet extends Thread {
-    DataInputStream receive = null;
-    JTextArea content = null;
-    public MessageGet(Socket socket, JTextArea txta) {
-        try {
-            receive = new DataInputStream(socket.getInputStream());
-        } catch(Exception e) {
-            System.err.println(e);
-        }
-        content = txta;
-    }
-    public void run() {
-        int a;
-        try {
-            while ((a = receive.read()) != -1) content.append(new Character((char)a).toString());
-        } catch(Exception e) {
-            System.err.println(e);
+        System.out.println("Starting Message System...");
+        Scanner in = new Scanner(System.in);
+        MessageClient mc = new MessageClient();
+        String input;
+        System.out.println(":System Started, type help for help.");
+        System.out.print(":");
+        while (true) {
+            input = in.nextLine();
+            if (input.equalsIgnoreCase("HELP")) {
+                mc.printHelp();
+                System.out.print(":");
+            } else if (input.equalsIgnoreCase("QUIT")) {
+                System.exit(0);
+            } else if (input.equalsIgnoreCase("CONNECT")) {
+                mc.connect(in);
+                in.nextLine();
+                System.out.print(":");
+            } else {
+                System.out.print("No command found.\n:");
+            }
         }
     }
-}
-
-class MessageSend {
-    DataOutputStream send = null;
-    public MessageSend(Socket socket) {
-        try {
-            send = new DataOutputStream(socket.getOutputStream());
-        } catch(Exception e) {
-            System.err.println(e);
-        }
+    public static void printHelp() {
+        System.out.println("help\tShow this prompt\nconnect\tStarts a new connection\nquit\tQuit the program\nexit\tExit a connection");
     }
-    public void send(String data) {
+    public void connect(Scanner in) {
+        Socket soc = null;
+        InetAddress addr = null;
+        System.out.print("IP_ADDRESS/HOST:");
+        String ip = in.nextLine();
+        System.out.print("PORT:");
+        int port = in.nextInt();
         try {
-            send.writeBytes(data + "\n");
+            System.out.println("Attempting to connect to HOST:\'" + ip + "\' on PORT:\'" + port + "\'");
+            addr = InetAddress.getByName(ip);
+            soc = new Socket(addr, port);
         } catch(Exception e) {
-            System.err.println(e);
+            System.out.println("Error connecting to server: " + e.getLocalizedMessage());
+            return;
         }
+        SwingUtilities.invokeLater(new MessageGUI(ip + ":" + port, soc));
     }
 }
 
 class MessageGUI implements Runnable {
-    Socket socket = null;
-    public MessageGUI(Socket soc) {
-        socket = soc;
-    }
-    public void run() {
-//        Setup the window.
-        JFrame window = new JFrame("Message");
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    public MessageGUI(String windowName, Socket server) {
+        JFrame window = new JFrame(windowName);
+        window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         window.setSize(500, 300);
         window.setLayout(new BorderLayout());
         window.setVisible(true);
-//        Setup the JTextArea for message output.
-        JTextArea content = new JTextArea();
-        content.setEditable(false);
-        new MessageGet(socket, content).start();
-        window.add(content, BorderLayout.CENTER);
-//        Setup the text input.
+        
+        MessageReceive mr = new MessageReceive(server);
+        mr.setEditable(false);
+        mr.setBackground(new Color(0, 0, 0));
+        mr.setForeground(new Color(0, 255, 0));
+        mr.setVisible(true);
+        new Thread(mr).start();
+        window.add(mr, BorderLayout.CENTER);
+        
+        DataOutputStream dos = null;
+        try {
+            dos = new DataOutputStream(server.getOutputStream());
+        } catch(Exception e) {
+            System.out.println("Error creating output stream to server: " + e.getLocalizedMessage());
+        }
+        
         JTextField input = new JTextField();
+        input.addActionListener(new MessageSend(server, input, dos));
+        input.setBackground(new Color(0, 0, 0));
+        input.setForeground(new Color(0, 255, 0));
         window.add(input, BorderLayout.PAGE_END);
-        input.addActionListener(new MessageField(input, socket));
+        
+        System.out.println("Displaying connection.");
+    }
+    public void run() {}
+}
+
+class MessageReceive extends JTextArea implements Runnable {
+    protected Socket server;
+    public MessageReceive(Socket server) {
+        this.server = server;
+    }
+    public void run() {
+        DataInputStream dis = null;
+        int bytes;
+        try {
+            dis = new DataInputStream(server.getInputStream());
+        } catch(Exception e) {
+            System.out.println("Error connecting server: " + e.getLocalizedMessage());
+        }
+        this.append("Connected.\n");
+        while (true) {
+            try {
+                while ((bytes = dis.read()) != -1) this.append(String.valueOf((char) bytes));
+            } catch(Exception e) {
+                System.out.println("Error reading from server: " + e.getLocalizedMessage());
+                return;
+            }
+        }
     }
 }
 
-class MessageField implements ActionListener {
-    Socket socket = null;
-    JTextField input = null;
-    public MessageField(JTextField in, Socket soc) {
-        input = in;
-        socket = soc;
+class MessageSend implements ActionListener {
+    protected Socket server;
+    protected JTextField input;
+    protected DataOutputStream dos = null;
+    public MessageSend(Socket server, JTextField input, DataOutputStream dos) {
+        this.server = server;
+        this.input = input;
+        this.dos = dos;
     }
     public void actionPerformed(ActionEvent ae) {
-        new MessageSend(socket).send(System.getProperty("user.name") + ":" + input.getText());
-        input.setText("");
+        try {
+            dos.writeBytes(input.getText() + "\n");
+            input.setText("");
+        } catch(Exception e) {
+            System.out.println("Error writing to server output stream: " + e.getLocalizedMessage());
+        }
     }
 }
--- MessageServer.java
+++ MessageServer.java
@@ -1,69 +1,92 @@
-//Server for JavaMessage project.
-//
-//Issues:
-//
-//MessageThread needs to share a InputStream between threads for messaging.
-
-import java.net.*;
 import java.io.*;
+import java.net.*;
 import java.util.*;
 
 public class MessageServer {
-
-    static final int PORT = 1234;
-
-    public static void main(String args[]) {
-        ServerSocket serverSocket = null;
-        Socket socket = null;
-
+    public static void main(String[] args) {
+        int port = Integer.parseInt(args[0]);
+        MessageServer ms = new MessageServer();
+        System.out.println("Starting server on port " + port + "...");
+        ServerSocket ss = null;
         try {
-            serverSocket = new ServerSocket(PORT);
-        } catch (Exception e) {
-            System.err.println(e);
+            ss = new ServerSocket(port);
+        } catch(Exception e) {
+            System.out.println("Error creating server: " + e.getLocalizedMessage());
+            System.exit(0);
+        }
+        System.out.println("Created server port, now waiting for users...");
+        Socket client = null;
+        DatagramSocket ds = null;
+        try {
+            ds = new DatagramSocket(4);
+        } catch(Exception e) {
+            System.out.println("IN:Error creating Datagram Server: " + e.getLocalizedMessage());
+            e.printStackTrace();
+            System.exit(0);
         }
         while (true) {
             try {
-                socket = serverSocket.accept();
-            } catch (Exception e) {
-                System.err.println(e);
+                client = ss.accept();
+                System.out.println("Connecting user: " + client.getInetAddress().toString());
+            } catch(Exception e) {
+                System.out.println("Error on server: " + e.getLocalizedMessage());
             }
-            // new thread for a client
-            new MessageThread(socket).start();
+            new MessageConnectionIn(client, ds).start();
+            new MessageConnectionOut(client, ds).start();
         }
     }
 }
 
-class MessageThread extends Thread {
-    protected Socket socket;
-    public MessageThread(Socket clientSocket) {
-        this.socket = clientSocket;
+class MessageConnectionOut extends Thread {
+    protected Socket client;
+    public DatagramSocket ds;
+    public MessageConnectionOut(Socket client, DatagramSocket ds) {
+        this.client = client;
+        this.ds = ds;
     }
     public void run() {
-        InputStream inp = null;
-        BufferedReader brinp = null;
-        DataOutputStream out = null;
+        this.setName(client.getInetAddress().getHostAddress() + ":OUT");
         try {
-            inp = socket.getInputStream();
-            brinp = new BufferedReader(new InputStreamReader(inp));
-        } catch (Exception e) {
-            System.err.println("USER left");
+            System.out.println("OUT:User connected.");
+            DataOutputStream dos = new DataOutputStream(client.getOutputStream());
+            while (true) {
+                byte[] outgoing = new byte[4096];
+                DatagramPacket dp = new DatagramPacket(outgoing, outgoing.length);
+                ds.receive(dp);
+                dos.writeChars(new String(outgoing) + "\n");
+            }
+        } catch(Exception e) {
+            System.out.println("OUT:Error connecting " + this.getName() + ": " + e.getLocalizedMessage());
             return;
         }
-        String line;
-        while (true) {
-            try {
-                line = brinp.readLine();
-                System.out.print(line + "\n");
-                if ((line == null) || line.equalsIgnoreCase("QUIT")) {
-                    socket.close();
-                    return;
-                } else {
-                    out.writeBytes(line + "\n\r");
-                    out.flush();
-                }
-            } catch (IOException e) {
-                return;
+    }
+}
+
+class MessageConnectionIn extends Thread {
+    protected Socket client;
+    public DatagramSocket ds;
+    public MessageConnectionIn(Socket client, DatagramSocket ds) {
+        this.client = client;
+        this.ds = ds;
+    }
+    public void run() {
+        this.setName(client.getInetAddress().getHostAddress() + ":IN");
+        try {
+            System.out.println("IN:User connected.");
+            BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            while (true) {
+                String lineIn = br.readLine();
+                byte[] input = lineIn.getBytes();
+                System.out.println(lineIn);
+                byte[] output = new byte[4096];
+                for (int c = 0; c < output.length; c++) output[c] = 0x0;
+                for (int i = 0; i < input.length && i < output.length; i++) output[i] = input[i];
+                DatagramPacket dp = new DatagramPacket(output, output.length, InetAddress.getLocalHost(), 4);
+                ds.send(dp);
             }
+        } catch(Exception e) {
+            System.out.println("IN:Error connecting to " + this.getName() + ": " + e.getLocalizedMessage());
+            return;
         }
     }
 }
